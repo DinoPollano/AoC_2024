b5bb6f2b868b587a2bebf64ba7ffa4256697b639
Dino Pollano <cpollano@gmail.com>
2024-12-17 23:09:09 +0000

    day_5 part 2 not working

--- day_5/src/data.rs
+++ day_5/src/data.rs
@@ -1,10 +1,16 @@
 use std::fs::read_to_string;
-use std::io::{self, BufRead};
+use std::io::{self};
 use std::path::PathBuf;
 
 pub fn load_test_data() -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
     let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     d.push("resources/data/test.txt");
+    load_list_file(d.as_path().to_str().unwrap()).unwrap()
+}
+
+pub fn load_actual_data() -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("resources/data/data.txt");
     load_list_file(d.as_path().to_str().unwrap()).unwrap()
 }
 
--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -8,17 +8,20 @@ pub fn sort_pages(order_vec: Vec<(u32, u32)>) -> HashMap<u32, HashSet<u32>> {
         order_map.entry(antecedant).or_default().insert(subsequent);
         order_map.entry(subsequent).or_default();
     }
-    for set in &order_map{
+    for set in &order_map {
         println!("{} : {:?}", set.0, set.1);
     }
     order_map
 }
 
-pub fn check_pages_are_correct(sorted_map: &HashMap<u32, HashSet<u32>>, updates: &Vec<u32>) -> bool {
+pub fn check_pages_are_correct(
+    sorted_map: &HashMap<u32, HashSet<u32>>,
+    updates: &Vec<u32>,
+) -> bool {
     let mut page = updates.iter().peekable();
     let mut correct: bool = true;
     while correct {
-        let page_number = match page.peek(){
+        let page_number = match page.peek() {
             Some(&value) => value,
             None => break,
         };
@@ -29,7 +32,7 @@ pub fn check_pages_are_correct(sorted_map: &HashMap<u32, HashSet<u32>>, updates:
             Some(set) => set,
             None => break,
         };
-        
+
         let tail: HashSet<u32> = page.clone().copied().collect();
         // println!("tail: {:?}", tail);
         // println!("order: {:?}", order);
@@ -38,23 +41,107 @@ pub fn check_pages_are_correct(sorted_map: &HashMap<u32, HashSet<u32>>, updates:
     correct
 }
 
-use data::load_test_data;
+pub fn sort_updates(sorted_map: &HashMap<u32, HashSet<u32>>, updates: &Vec<u32>) -> Vec<u32> {
+    let mut page_tails: Vec<(usize, u32)> = updates
+        .clone()
+        .into_iter()
+        .map(|x| {
+            let length = match sorted_map.get(&x) {
+                Some(set) => set.len(),
+                None => 0,
+            };
+            (length, x)
+        })
+        .collect();
+    page_tails.sort_by(|a, b| (a.0.cmp(&b.0).reverse()));
+    page_tails.iter().map(|x| x.1).collect()
+}
+
+pub fn sort_updates2(sorted_map: &HashMap<u32, HashSet<u32>>, updates: &Vec<u32>) -> Vec<u32> {
+    let mut sorted: Vec<u32> = updates.clone();
+    let mut page = sorted.iter().skip(sorted.len()).peekable();
+    loop {
+       let mut page_number= match page.peek() {
+        Some(&value) => value,
+        None => break,
+       };
+
+        let order = match sorted_map.get(&page_number) {
+            Some(set) => set,
+            None => break,
+        };
+       let outliter =  page.position(|x|{!order.contains(x)});
+       match outliter{
+        Some(u32) => {
+            let number = page.remove(outliter.unwrap());
+            page.next_back();
+        },
+        None => {
+            page.next_back();
+        },
+
+       };
+        
+    }
+
+    sorted
+}
+
+use data::{load_actual_data, load_test_data};
 fn main() {
     println!("DAY 5");
-    let (order_vec, updates_vec) = load_test_data();
+    let (order_vec, updates_vec) = load_actual_data();
+    let sorted_order = sort_pages(order_vec);
+    let mut result: u32 = 0;
+    let mut result_of_sorted: u32 = 0;
+    for updates in updates_vec {
+        if check_pages_are_correct(&sorted_order, &updates) {
+            let middle_page_num = updates.len() / 2;
+            result += updates.get(middle_page_num).unwrap();
+        } else {
+            println!("{:?}", updates);
+            let sorted_update = sort_updates2(&sorted_order, &updates);
+            let middle_page_num = (sorted_update.len()) / 2;
+            result_of_sorted += sorted_update.get(middle_page_num).unwrap();
+        }
+    }
 
-    println!("{:?}", sort_pages(order_vec));
+    println!("unsorted: {}", result);
+    println!("sorted: {}", result_of_sorted);
+    print!("summed: {}", (result + result_of_sorted))
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{check_pages_are_correct, data::load_test_data, sort_pages};
+    use crate::{check_pages_are_correct, data::load_test_data, sort_pages, sort_updates};
     #[test]
     fn test_check_pages_are_correct() {
         let (order_vec, updates_vec) = load_test_data();
-        let sorted = sort_pages(order_vec );
-        for (i, expected_result) in vec![true, true, true, false, false, false].iter().enumerate(){
-        assert_eq!(check_pages_are_correct(&sorted, &updates_vec[i]), *expected_result, "{:?}",&updates_vec[i]);
+        let sorted = sort_pages(order_vec);
+        for (i, expected_result) in vec![true, true, true, false, false, false]
+            .iter()
+            .enumerate()
+        {
+            assert_eq!(
+                check_pages_are_correct(&sorted, &updates_vec[i]),
+                *expected_result,
+                "{:?}",
+                &updates_vec[i]
+            );
         }
+    }
+    #[test]
+    fn test_sort_updates() {
+        let (order_vec, updates_vec) = load_test_data();
+        let sorted = sort_pages(order_vec);
+        assert_eq!(
+            sort_updates(&sorted, &(vec![75, 97, 47, 61, 53])),
+            vec![97, 75, 47, 61, 53]
+        );
+        assert_eq!(sort_updates(&sorted, &(vec![61, 13, 29])), vec![61, 29, 13]);
+        assert_eq!(
+            sort_updates(&sorted, &(vec![97, 13, 75, 29, 47])),
+            vec![97, 75, 47, 29, 13]
+        );
     }
 }
